a107055c8175e79eeeda2c95c69c832221899082
Steve <mchess432@gmail.com>
2020-06-12 11:15:22 +0300

    Added onclick on favorites button

--- app/src/main/java/pefasouthb/org/Favorites.java
+++ app/src/main/java/pefasouthb/org/Favorites.java
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Favorites extends Fragment {
+
+    Button signIn;
     public static Favorites newInstance() {
         Favorites fragment = new Favorites();
         return fragment;
@@ -27,7 +31,15 @@ public class Favorites extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+        signIn = view.findViewById(R.id.btnSignIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Functionality coming soon", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 
 }
